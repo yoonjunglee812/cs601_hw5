f07aceb6b5cbbe7b3581cd9e21602a0035495338
yoonjunglee812 <yoonjung.lee812@gmail.com>
2024-02-26 22:39:46 -0500

    first commit

---
+++ sbatch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --partition=mig_class
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=2:00:0
+#SBATCH --job-name="CS 601.471/671 homework5"
+#SBATCH --output=slurm-%j.out
+#SBATCH --mem=16G
+
+# add module only when you run this code on rockfish
+module load anaconda 
+source ~/.bashrc
+conda activate ssm_hw5 # activate the Python environment
+
+# runs your code
+python -u main.py
